5a0990121014544b5d91dd08b103df285aab5ba0
amanjha491 <aman4sep1@gmail.com>
2025-11-02 11:30:40 +0530

    Activity Springboot initialize

---
+++ activitiyService/src/main/java/com/fitness/activitiyService/ActivitiyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.metaFitAI.activitiyService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiyServiceApplication.class, args);
+	}
+
+}

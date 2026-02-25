ea93509390927249336559506063ca931edeb9e0
amanjha491 <aman4sep1@gmail.com>
2026-02-26 03:08:07 +0530

    add Outh2 auth using KeyCloak

---
+++ userService/src/main/java/com/metaFitAi/userService/MetaFitAiApplication.java
@@ -0,0 +1,13 @@
+package com.metaFitAi.userService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaFitAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaFitAiApplication.class, args);
+	}
+
+}
---
+++ userService/src/test/java/com/metaFitAi/userService/MetaFitAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaFitAi.userService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaFitAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
